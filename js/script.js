f02b35bc39ebf309a0474bc3987de27a50dcbf04
Дмитрий <dkovalkov731@gmail.com>
2025-10-28 18:57:19 +0300

    nitial commit: ortfolio website with , , avacript

---
+++ js/script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const themeBtn = document.querySelector('.theme-btn');
+    
+    themeBtn.addEventListener('click', function() {
+        document.body.classList.toggle('dark-theme');
+        
+        if (document.body.classList.contains('dark-theme')) {
+            themeBtn.textContent = '☀️ theme';
+        } else {
+            themeBtn.textContent = '🌙 theme';
+        }
+    });
+});
+
